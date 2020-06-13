e58313ad667d8131f14e665f48ee4c654538542a
Stephan <fischer.stephanleo@tum.de>
2020-06-13 14:12:12 +0200

    title screen edited with functional buttons

--- Scenes/Game/returnToTitle.gd
+++ Scenes/Game/returnToTitle.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Title.tscn")
+
+
+func _on_Test_pressed():
+	get_tree().change_scene("res://Scenes/TestGround.tscn")
