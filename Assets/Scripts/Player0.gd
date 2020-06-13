e33947917134fdf370b17b93f0edc2a01958b2bb
phil <moersler@in.tum.de>
2020-06-13 02:31:51 +0200

    player tweaks. still not usable

--- Assets/Scripts/Player0.gd
+++ Assets/Scripts/Player0.gd
@@ -27,7 +27,39 @@ func move():
 		if is_on_floor():
 			$AnimatedSprite.play("idle")#walk
 			$AnimatedSprite.speed_scale = Input.get_action_strength("ui_right")
-		$AnimatedSprite.flip_h = 0
+		$AnimatedSprite.flip_h = 1
 	elif Input.is_action_pressed("ui_left"):
-		flip = true
+		flip = false
+		motion.x = min(motion.x-ACCELERATION, -SPEED)*Input.get_action_strength("ui_left")
+		if is_on_floor():
+			$AnimatedSprite.play("idle")#walk
+			$AnimatedSprite.speed_scale = Input.get_action_strength("ui_left")
+		$AnimatedSprite.flip_h = 0
+	else:
+		friction = true
+	pass
+	
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	motion.y = min(motion.y, 100)	
+	
+	move()
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMPFORCE
+		if friction == true:
+			motion.x = lerp(motion.x,0,0.3)
+	else:
+		if friction == true:
+			motion.x = lerp(motion.x,0,0.005)
+		if motion.y < 0:
+			$AnimatedSprite.play("idle")
+			 #BUG: Animation not playing, just still image
+		if motion.y > 0:
+			$AnimatedSprite.play("idle")
+			
+	
+	motion = move_and_slide(motion, UP)
+	
 	pass
