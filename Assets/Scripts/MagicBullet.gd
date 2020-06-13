61e6221cbd29d7738e5a94e73d10c8d149da0d70
phil <moersler@in.tum.de>
2020-06-13 20:55:07 +0200

    koj

---
+++ Assets/Scripts/MagicBullet.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var speed = 200
+var angle = 0
+var timer = 10
+var dmg = 10
+var motion = Vector2()
+
+func setDirSimple(var dir):
+	if dir == 1:
+		speed = abs(speed)
+	else:
+		speed = -abs(speed)
+
+func setDir(var v):
+	motion = v
+	pass
+
+func setAngle(var alpha):
+	angle = alpha
+
+func _ready():
+	
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if timer > 0:
+		timer -= delta
+	else:
+		queue_free()
+		
+	translate(speed*motion*delta)
+	pass
