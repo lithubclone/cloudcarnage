d0c1817041e2cd8abd8d4d879c26aa9eb263d1af
Stephan <fischer.stephanleo@tum.de>
2020-06-14 02:36:35 +0200

    modification scene updated

--- Scenes/Game/TopContainer.gd
+++ Scenes/Game/TopContainer.gd
@@ -14,66 +14,66 @@ var w = ["Pistol", "Crossbow", "MG"]
 var p = ["MagicBullet", "Mine"]
 
 func _ready():
-	for i in range(m.size()):
-		option11.add_item(m[i])
-		option21.add_item(m[i])
 	for i in range(w.size()):
-		option12.add_item(w[i])
-		option22.add_item(w[i])
+		option11.add_item(w[i])
+		option21.add_item(w[i])
+	for i in range(m.size()):
+		option12.add_item(m[i])
+		option22.add_item(m[i])
 	for i in range(p.size()):
 		option13.add_item(p[i])
 		option23.add_item(p[i])
 
 
 func _on_weapon1_item_selected(ID):
-	match option11:
-		"Pistol":
+	match ID:
+		0:
 			global.weapon2 = global.PISTOL
-		"Crossbow":
+		1:
 			global.weapon2 = global.CROSSBOW
-		"MG":
+		2:
 			global.weapon2 = global.MG
 
 func _on_magic1_item_selected(ID):
-	match option12:
-		"Flame":
+	match ID:
+		0:
 			global.element2 = global.FLAME
-		"Water":
+		1:
 			global.element2 = global.WATER
-		"Earth":
+		2:
 			global.element2 = global.EARTH
 			
 func _on_projectile1_item_selected(ID):
-	match option13:
-		"MagicBullet":
+	match ID:
+		0:
 			global.element2 = global.FLAME
-		"Mine":
+		1:
 			global.element2 = global.WATER
 
 
 func _on_weapon2_item_selected(ID):
-	match option21:
-		"Pistol":
+	match ID:
+		0:
 			global.weapon3 = global.PISTOL
-		"Crossbow":
+		1:
 			global.weapon3 = global.CROSSBOW
-		"MG":
-			global.weapon3
+		2:
+			global.weapon3 = global.MG
 
 
 func _on_magic2_item_selected(ID):
-	match option22:
-		"Flame":
+	match ID:
+		0:
 			global.element3 = global.FLAME
-		"Water":
+		1:
 			global.element3 = global.WATER
-		"Earth":
+		2:
 			global.element3 = global.EARTH
 
 
 func _on_projectile2_item_selected(ID):
-	match option23:
-		"MagicBullet":
+	match ID:
+		0:
 			global.element3 = global.FLAME
-		"Mine":
+		1:
 			global.element3 = global.WATER
